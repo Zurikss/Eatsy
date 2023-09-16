1b064c93e8b60a090b7816afec67d6acf394a774
Zurikss <victorander18@gmail.com>
2023-09-16 14:47:16 +0100

    Initial Commit

---
+++ Eatsy/EatsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatsyApp.swift
+//  Eatsy
+//
+//  Created by Victor on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct EatsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
